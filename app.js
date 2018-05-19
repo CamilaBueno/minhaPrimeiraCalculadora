b96996057d24ebaf7de24849da37bea5a48b834a
Camila Bueno <cbf.camila@gmail.com>
2018-05-19 12:07:04 -0300

    formatação e retirada de comentários

--- app.js
+++ app.js
@@ -1,37 +1,34 @@
-//criar 2 textbox que receberam 2 números
-//botões de mais, menos, vezes e dividir
-//imprimir resultado
-function Somar1(){
+function Somar1() {
   var n1 = document.getElementById("n1").value;
-  var n2= document.getElementById("n2").value;
+  var n2 = document.getElementById("n2").value;
   var resultado = parseFloat(n1) + parseFloat(n2);
   document.getElementById('result').textContent = resultado;
 
 }
-function Subtrair1(){
+function Subtrair1() {
   var n1 = document.getElementById("n1").value;
-  var n2= document.getElementById("n2").value;
+  var n2 = document.getElementById("n2").value;
   var resultado = parseFloat(n1) - parseFloat(n2);
   document.getElementById('result').textContent = resultado;
 
 }
-function Multiplicar1(){
+function Multiplicar1() {
   var n1 = document.getElementById("n1").value;
-  var n2= document.getElementById("n2").value;
+  var n2 = document.getElementById("n2").value;
   var resultado = parseFloat(n1) * parseFloat(n2);
   document.getElementById('result').textContent = resultado;
 
 }
-function Dividir1(){
+function Dividir1() {
   var n1 = document.getElementById("n1").value;
-  var n2= document.getElementById("n2").value;
+  var n2 = document.getElementById("n2").value;
   var resultado;
-  if(n2==0){
+  if (n2 == 0) {
     resultado = "Não é possível dividir um número por zero!";
-  }else{
+  } else {
     resultado = parseFloat(n1) / parseFloat(n2);
 
   }
-document.getElementById('result').textContent = resultado;
+  document.getElementById('result').textContent = resultado;
 
 }
